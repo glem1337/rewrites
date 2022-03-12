2e44f8196aff75dc684d4c95dbfc5e1ca1769a67
Evhenii Hryhoriev <evhenii.hryhoriev@rubygarage.org>
2022-03-12 14:26:06 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/blog/:slug',
+        destination: 'https://blog.vancation.com/:slug',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
