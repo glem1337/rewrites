0d9f9e2705af649fcd170f23dbde1a5315bbab6a
Evhenii Hryhoriev <evhenii.hryhoriev@rubygarage.org>
2022-03-13 18:38:45 +0200

    update congif for rewrites

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
   async rewrites() {
     return [
-      {
-        source: '/blog/:slug',
-        destination: 'https://blog.vancation.com/:slug',
-      },
+	    {
+		    source: '/blog/:slug*/',
+		    destination: 'https://blog.vancation.com/:slug*/',
+	    },
     ]
   },
 }
